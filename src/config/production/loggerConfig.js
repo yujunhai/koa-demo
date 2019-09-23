b97f735826801085f788beb82f816202e7eafdab
catherine201 <303673522@qq.com>
2019-09-23 17:18:38 +0800

    production log

--- src/config/production/loggerConfig.js
+++ src/config/production/loggerConfig.js
@@ -10,8 +10,8 @@ module.exports = {
     streams: [
       {
           type: 'file',
-          path: path.join('/var/log/server/', 'logs/log-%Y-%m-%d.log'),
-        //   path: `${process.cwd()}/logs/log-${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}.log`,
+        //   path: path.join('/var/log/server/', 'logs/log-%Y-%m-%d.log'),
+          path: `/var/log/server/logs/log-${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}.log`,
           period: '1d',          // daily rotation
           totalFiles: 100,       // keep 10 back copies
           rotateExisting: true,  // Give ourselves a clean file when we start up, based on period
