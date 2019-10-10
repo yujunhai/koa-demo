caa5065aa2565acf6a474d71ab0acfdb1dd05baf
catherine201 <303673522@qq.com>
2019-10-10 17:06:11 +0800

    revise addr

--- src/common/define.js
+++ src/common/define.js
@@ -1,9 +1,7 @@
 "use strict";
-
-const env = process.env.NODE_ENV || 'development';
 module.exports = {
   redisPrefix: {
     ACCESS_TOKEN_KEY_PREFIX: "AT:"
   },
-  serverIp: env === 'development' ? 'http://192.168.0.50:3030' : 'http://yrbing.com.cn:3030'
+  serverIp: 'http://yrbing.com.cn:3030'
 };
